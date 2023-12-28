e693af4d0f23b57227b740b53ac1e39b2f80da60
fahim <fahimahamedweb@gmail.com>
2023-12-28 11:13:55 +0600

    image issue fixed

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -35,8 +35,9 @@ class ProductController extends Controller
         
         $name_gen = hexdec(uniqid()).'.'.$request->file('product_thumbnail')->getClientOriginalExtension();
         $image = $manager->read($request->file('product_thumbnail'));
-        
-        $image->resize(1100,1100)->save(storage_path('app/public/'.$path.'/'.$name_gen));
+        $path = storage_path();
+        dd($path);
+        $image->resize(1100,1100)->save(storage_path('app/public/'.$name_gen));
         $save_url = '/'.$name_gen;
 
         $product_id = Product::insertGetId([
