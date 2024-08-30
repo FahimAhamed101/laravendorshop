fe00ed49c034b4aa49d0a4c52384fe7690b5f763
fahim <fahimahamedweb@gmail.com>
2024-08-31 04:46:06 +0600

    vercel done

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Category;
 use App\Models\Seo;
 use App\Models\SiteSetting;
 use Illuminate\Support\Facades\Hash;
@@ -21,7 +22,24 @@ class UserSeeder extends Seeder
             'password' => Hash::make('admin'),
             'role' => 'admin'
         ]);
- 
+        $category = Category::create([
+            'category_name'     => 'Tshirt',
+            'category_slug'    => 'tshirt',
+            'category_image' => '/admin/assets/cat-1.png',
+            
+        ]);
+        $category = Category::create([
+            'category_name'     => 'IPAD',
+            'category_slug'    => 'ipad',
+            'category_image' => '/admin/assets/cat-1.png',
+            
+        ]);
+        $category = Category::create([
+            'category_name'     => 'shoes',
+            'category_slug'    => 'shoes',
+            'category_image' => '/admin/assets/cat-1.png',
+            
+        ]);
         $seos = Seo::create([
             'meta_title'     => 'Admin',
             
